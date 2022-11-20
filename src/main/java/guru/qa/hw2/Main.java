305536ae910b1ceae9cc52cdc9032d44e9fc8319
Novikov Aleksei <3900676@gmail.com>
2022-11-20 22:53:55 +0300

    added MAIN

---
+++ src/main/java/guru/qa/hw2/Main.java
@@ -0,0 +1,13 @@
+package guru.qa.hw2;
+
+public class Main {
+    public static void main(String[] args) {
+        Storehouse storehouse = new Storehouse();
+        storehouse.printAllProduct();
+        storehouse.addNewItem("Bread", 10);
+        storehouse.addNewItem("Tomato", 3);
+        storehouse.printAllProduct();
+        storehouse.findProduct("Tomato");
+        storehouse.findProduct("Snickers");
+    }
+}
