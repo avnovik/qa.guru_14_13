d246b1672fc825b08cfb3a2029dc8085d6ebadcb
Novikov Aleksei <3900676@gmail.com>
2022-11-20 22:53:13 +0300

    added Storehouse

---
+++ src/main/java/guru/qa/hw2/Storehouse.java
@@ -0,0 +1,34 @@
+package guru.qa.hw2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Storehouse {
+    
+    Map<String, Integer> storehouse = new HashMap<>();
+
+
+    void addNewItem(String name, int num) {
+        storehouse.put(name, num);
+    }
+
+    void printAllProduct() {
+        if (storehouse.isEmpty()) {
+            System.out.println("Warehouse is empty");
+        } else {
+            for (Map.Entry<String, Integer> pair : storehouse.entrySet()) {
+                String key = pair.getKey();
+                int value = pair.getValue();
+                System.out.println(key + ": " + value);
+            }
+        }
+    }
+
+    void findProduct(String name) {
+        if (storehouse.containsKey(name)) {
+            System.out.println("The product \"" + name + "\" is in stock.");
+        } else {
+            System.out.println("Product out of stock.");
+        }
+    }
+}
